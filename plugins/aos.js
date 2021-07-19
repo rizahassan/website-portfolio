9809837b0b7139524912fdde033420211b2f9357
Riza Hassan <55040840+rizahassan@users.noreply.github.com>
2021-07-19 03:58:17 -0500

    Added Animation On Scroll package

---
+++ plugins/aos.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+export default ({ app }) => {
+    app.AOS = new AOS.init({ disable: "phone" }); // eslint-disable-line new-cap
+  };
